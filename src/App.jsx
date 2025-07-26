09100469a016e77618edcd667c62532919dcda89
Hoàng Đức Quang <hdquang6@gmail.com>
2025-07-26 08:16:07 +0700

    b28

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const MyComponent = () => {
+  return (
+    <h2>Hello Quang</h2>
+  );
+}
+
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -17,6 +23,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
