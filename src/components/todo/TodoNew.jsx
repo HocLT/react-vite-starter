5b7fcfc3a77509bb16abadf4706067e35d5487b8
Hoàng Đức Quang <hdquang6@gmail.com>
2025-07-29 13:52:11 +0700

    b26

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,25 @@
 const TodoNew = (props) => {
   const { addNewTodo } = props;
   // addNewTodo('Quang');
+
+  const handleClick = () => {
+    alert("click me");
+  }
+
+  const handleChange = (msg) => {
+    console.log("click me: " + msg);
+  }
+
   return (
     <div className='todo-new'>
-      <input type="text" />
-      <button>Add</button>
+      <input
+        type="text"
+        onChange={(evt) => handleChange(evt.target.value)}
+      />
+      <button
+        style={{ cursor: "pointer" }}
+        onClick={addNewTodo}
+      >Add</button>
     </div>
   );
 };
